d54b7a2a841751d9a5e9e49dc3e51b447e7863b7
Dikshant <dikshant.dak@gmail.com>
2023-07-15 12:20:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="text-3xl font-semibold">
+        App
+    </div>
+  )
+}
+
+export default App;
